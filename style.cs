deca1bb896906b0406a3f19e7f5a3d155b311af7
fleyta <44262254+fleyta@users.noreply.github.com>
2018-11-29 13:40:43 +0300

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,24 @@
+<style>
+  BODY {
+  align-items: left;
+  justify-content: left;
+  background: white;
+  font-family: Georgia;
+}
+fl {
+  font-size: 50px;
+  font-weight: normal;
+  cursor: pointer;
+  overflow: hidden;
+  text-shadow: 0 0 tomato,
+               0 0 yellowgreen,
+               0 0 skyblue;
+  color: transparent;
+  transition: all 1s;
+  }
+fl:hover {
+    text-shadow: -400px 0 tomato,
+                 400px 0 yellowgreen,
+                 0 0 skyblue;
+    }
+</style>
